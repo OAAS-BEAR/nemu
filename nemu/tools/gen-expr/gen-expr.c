a31c1577b966d01564920b9ad7917da3c42ed0ad
tracer-ics2019 <tracer@njuics.org>
2022-01-04 07:18:45 +0800

    >  run U201711354 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  07:18:45 up 1 day,  2:33,  1 user,  load average: 0.08, 0.02, 0.01 71a0b5a862e4a6eaf62f789aef27d73b38efff6

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -3,15 +3,86 @@
 #include <stdlib.h>
 #include <time.h>
 #include <assert.h>
+#include <stdbool.h>
 #include <string.h>
+static bool gen_success;
 
 // this should be enough
-static char buf[65536];
-static inline void gen_rand_expr() {
-  buf[0] = '\0';
+static char buf[99999];
+uint32_t choose(uint32_t num){
+	return rand()%num;
+}
+void strcatt(char* buf, char*s){
+	if(strlen(buf)+strlen(s)<99999)
+		strcat(buf,s);
+	else
+	   gen_success=false;
+}
+void gen_num(){
+	uint32_t value=rand()%20;
+	char string[8];
+	sprintf(string,"%u",value);
+	strcatt(buf,string);
+}
+void gen_left(){
+	strcatt(buf,"(");
+}
+void gen_right(){
+	strcatt(buf,")");
+}
+void gen_operator(){
+	switch(choose(4)){
+		case 0:
+		       	strcatt(buf,"+");
+			break;
+                case 1:
+                        strcatt(buf,"-");
+                        break;
+
+                case 2:
+                        strcatt(buf,"*");
+                        break;
+
+                case 3:
+                        strcatt(buf,"/");
+                        break;
+/*
+                case 4:
+                        strcat(buf,"!=");
+                        break;
+                case 5:
+                        strcat(buf,"==");
+                        break;
+                case 6:
+                        strcat(buf,"&&");
+                        break;*/
+	}
 }
 
-static char code_buf[65536];
+
+
+
+
+
+static inline void gen_rand_expr() {
+  switch(choose(3)){
+	  case 0:
+		  gen_num();
+		  break;
+	  case 1:
+		  gen_left();
+		  gen_rand_expr();
+		  gen_right();
+		  break;
+	case 2:
+		  gen_rand_expr();
+		  gen_operator();
+		  gen_rand_expr();
+		  break;
+  }
+}
+
+static char code_buf[99999];
 static char *code_format =
 "#include <stdio.h>\n"
 "int main() { "
@@ -23,24 +94,45 @@ static char *code_format =
 int main(int argc, char *argv[]) {
   int seed = time(0);
   srand(seed);
+  int h=0;
   int loop = 1;
   if (argc > 1) {
     sscanf(argv[1], "%d", &loop);
   }
   int i;
   for (i = 0; i < loop; i ++) {
+    memset(buf,0,99999);
+    gen_success=true;
     gen_rand_expr();
-
+    if(!gen_success)
+	    continue;
     sprintf(code_buf, code_format, buf);
 
     FILE *fp = fopen("/tmp/.code.c", "w");
     assert(fp != NULL);
     fputs(code_buf, fp);
+  //  printf("%s",code_buf);
     fclose(fp);
+ //      system("gcc /tmp/.code.c -o /tmp/.expr > out");
+   FILE *f=popen("gcc -W /tmp/.code.c -o /tmp/.expr","r");
+   if(f==NULL){
+     exit(-1);
+   :  }
+    char warning[100];
+    memset(warning,0,100);
+    while(fgets(warning,sizeof(warning),f)!=NULL){
+       printf("%d\n",strlen(warning));
 
-    int ret = system("gcc /tmp/.code.c -o /tmp/.expr");
-    if (ret != 0) continue;
-
+       if(strlen(warning)!=0){
+	    printf("%s",warning);
+	    break;
+    }
+    }
+  
+    pclose(f);
+    if(strlen(warning)!=0)
+	    continue;
+    
     fp = popen("/tmp/.expr", "r");
     assert(fp != NULL);
 
@@ -50,5 +142,6 @@ int main(int argc, char *argv[]) {
 
     printf("%u %s\n", result, buf);
   }
+//  printf("%d\n",h);
   return 0;
 }
