5de761ba08ba4105b27fdb0ec2a0b10d3ea16cef
tracer-ics2019 <tracer@njuics.org>
2022-01-06 02:57:59 +0800

    >  run U201711354 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  02:57:59 up 1 day, 22:32,  1 user,  load average: 0.26, 0.13, 0.04 fb21dba17f496d539af63f83b69e70112f4d69cf

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -3,39 +3,102 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  return 0;
+  size_t l=0;
+  while(*s!=0){
+    l++;
+    s++;
+    }
+  return l;
 }
 
 char *strcpy(char* dst,const char* src) {
-  return NULL;
+  char * cur=dst;
+  while(*src!=0){
+    *dst=*src;
+    dst++;
+    src++;
+    }
+    *dst='\0';
+  return cur;
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
-  return NULL;
+  char * cur=dst;
+  while((n--)&&((*dst++ =*src++)!='\0'));
+  if(n>0){
+   while(--n){
+   *dst++='\0';
+   }
+   }
+  return cur;
 }
 
 char* strcat(char* dst, const char* src) {
-  return NULL;
+  char * cur=dst;
+  while(*dst)
+   dst++;
+  while((*dst++ = *src++)!='\0')
+  ;
+  
+  return cur;
 }
 
 int strcmp(const char* s1, const char* s2) {
-  return 0;
+
+  while(*s1 ==*s2){
+  if(*s1=='\0')
+     return 0;
+   s1++;
+   s2++;
+     }
+   if((*s1-*s2)>0)
+   return 1;
+  return -1;
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
+  unsigned char c1,c2;
+  while(n){
+  c1=*s1++;
+  c2=*s2++;
+  if(c1!=c2)
+  return c1<c2? -1:1;
+  if(!c1)
+  break;
+  n--;
+  }
   return 0;
 }
 
-void* memset(void* v,int c,size_t n) {
-  return NULL;
+void* memset(void* v,int c,size_t n) { 
+   char * cur=v;
+   while(n--){
+   *cur++=c;
+   }
+  return v;
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
-  return NULL;
+    char *tmp=out;
+    const char *s=in;
+    while(n--){
+    *tmp++ = *s++;
+    }
+    return out;
 }
 
 int memcmp(const void* s1, const void* s2, size_t n){
-  return 0;
+  unsigned char * su1,*su2;
+  int res=0;
+  for(su1=s1,su2=s2;n>0;++su1,++su2,n--){
+  if((res=*su1-*su2)!=0)
+    break;
+    }
+   return res;
+   
+  
+  
+
 }
 
 #endif
