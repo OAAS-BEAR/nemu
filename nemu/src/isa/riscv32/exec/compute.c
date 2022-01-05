f66a71d52abe2142e2f8e8f8a90cd3dfb5e0d516
tracer-ics2019 <tracer@njuics.org>
2022-01-05 20:42:58 +0800

    >  compile U201711354 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:42:58 up 1 day, 16:20,  1 user,  load average: 0.14, 0.14, 0.06 20a3ef78a1b424c256a3d21a3f78aa33ba7dea36

--- nemu/src/isa/riscv32/exec/compute.c
+++ nemu/src/isa/riscv32/exec/compute.c
@@ -76,21 +76,23 @@ make_EHelper(ri){
           case 4:
                 imm=id_src2->val;
                 imm=imm<<20>>20;
-                result;                
+             //   result;                
                 rtl_xor(&result,&imm,&id_src->val);
                 rtl_sr(id_dest->reg,&result,4);
-                break;         
+                break;    
+          case 5:
+                     
           case 6:
                 imm=id_src2->val;
                 imm=imm<<20>>20;
-                result;                
+               // result;                
                 rtl_or(&result,&imm,&id_src->val);
                 rtl_sr(id_dest->reg,&result,4);
                 break;                
           case 7:
                 imm=id_src2->val;
                 imm=imm<<20>>20;
-                result;                
+              //  result;                
                 rtl_and(&result,&imm,&id_src->val);
                 rtl_sr(id_dest->reg,&result,4);
                 break;          
@@ -143,24 +145,31 @@ make_EHelper(b){
               
               
 make_EHelper(r){      
-     uint32_t usrc;
+      uint32_t usrc;
       int result;
       int32_t src;
       int32_t src2;
       uint32_t usrc2;
+      int shift;
       switch(decinfo.isa.instr.funct3){
           case 0:
-                                
+                if(decinfo.isa.instr.funct7==0){              
                 rtl_add(&result,&id_src2->val,&id_src->val);
+                rtl_sr(id_dest->reg,&result,4);}
+                else{
+                 rtl_sub(&result,&id_src2->val,&id_src->val);
+                 rtl_sr(id_dest->reg,&result,4);}
+                break;
+          case 1:
+                shift=id_src2->val & 0x0000001f;
+                rtl_shl(&result,&id_src->val,&shift);
                 rtl_sr(id_dest->reg,&result,4);
                 break;
           case 2:
-                imm=id_src2->val;
-                imm=imm<<20>>20;
-                
-                r_value=id_src->val;
+                src=id_src->val;
+                src2=id_src2->val;
                 result=0;
-                if(r_value<imm){
+                if(src<src2){
                    result =1;
                    rtl_sr(id_dest->reg,&result,4);
                    }
@@ -170,38 +179,39 @@ make_EHelper(r){
                   break; 
                    
           case 3:
-                imm=id_src2->val;
-                imm=imm<<20>>20;
-                uimm=imm;
-                ur_value=id_src->val;
+                usrc=id_src->val;
+                usrc2=id_src2->val;
                 result=0;
-                if(ur_value<uimm){
+                if(usrc<usrc2){
                    result =1;
                    rtl_sr(id_dest->reg,&result,4);
                    }
                 else{
                    rtl_sr(id_dest->reg,&result,4);
                    }
-                  break;
-          case 4:
-                imm=id_src2->val;
-                imm=imm<<20>>20;
-                result;                
-                rtl_xor(&result,&imm,&id_src->val);
+                  break; 
+          case 4:             
+                rtl_xor(&result,&id_src2->val,&id_src->val);
                 rtl_sr(id_dest->reg,&result,4);
-                break;         
-          case 6:
-                imm=id_src2->val;
-                imm=imm<<20>>20;
-                result;                
-                rtl_or(&result,&imm,&id_src->val);
+                break;       
+          case 5:
+                if(decinfo.isa.instr.funct7==0){              
+                shift=id_src2->val & 0x0000001f;
+                rtl_shr(&result,&id_src->val,&shift);
+                rtl_sr(id_dest->reg,&result,4);
+                }
+                else{
+                shift=id_src2->val & 0x0000001f;
+                rtl_sar(&result,&id_src->val,&shift);
+                rtl_sr(id_dest->reg,&result,4);
+                 }
+                break;           
+          case 6:             
+                rtl_or(&result,&id_src2->val,&id_src->val);
                 rtl_sr(id_dest->reg,&result,4);
                 break;                
-          case 7:
-                imm=id_src2->val;
-                imm=imm<<20>>20;
-                result;                
-                rtl_and(&result,&imm,&id_src->val);
+          case 7:          
+                rtl_and(&result,&id_src2->val,&id_src->val);
                 rtl_sr(id_dest->reg,&result,4);
                 break;          
 }
