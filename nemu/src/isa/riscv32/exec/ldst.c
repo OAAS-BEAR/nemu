036fc43876a14fa09e0f2fc64e87a7211febf441
tracer-ics2019 <tracer@njuics.org>
2022-01-05 21:55:47 +0800

    >  compile U201711354 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:55:47 up 1 day, 17:32,  1 user,  load average: 0.28, 0.09, 0.06 5b36d6923d80fafb25019a048331efece3bcbbf

--- nemu/src/isa/riscv32/exec/ldst.c
+++ nemu/src/isa/riscv32/exec/ldst.c
@@ -1,13 +1,45 @@
 #include "cpu/exec.h"
 
 make_EHelper(ld) {
-  rtl_lm(&s0, &id_src->addr, decinfo.width);
-  rtl_sr(id_dest->reg, &s0, 4);
 
   switch (decinfo.width) {
-    case 4: print_asm_template2(lw); break;
-    case 2: print_asm_template2(lhu); break;
-    case 1: print_asm_template2(lbu); break;
+    
+    case 4: 
+      rtl_lm(&s0, &id_src->addr, decinfo.width);
+      rtl_sr(id_dest->reg, &s0, 4);
+    print_asm_template2(lw); break;
+    case 2: 
+     if(decinfo.isa.instr.funct3==1){
+     // lh
+     rtl_lm(&s0,&id_src->addr,2);
+     int result=s0;
+     result=result<<16>>16;
+     }
+     else{
+     // lhu
+       rtl_lm(&s0, &id_src->addr, decinfo.width);
+       rtl_sr(id_dest->reg, &s0, 4);
+     
+     print_asm_template2(lhu);
+     }
+     break;
+    case 1: 
+    if(decinfo.isa.instr.funct3==0){
+    //lb
+      rtl_lm(&s0,&id_src->addr,1);
+     int result=s0;
+     result=result<<24>>24;     
+    
+    }
+    else{
+    //lbu
+        rtl_lm(&s0, &id_src->addr, decinfo.width);
+       rtl_sr(id_dest->reg, &s0, 4);
+        
+    
+     print_asm_template2(lbu);
+    }    
+    break;
     default: assert(0);
   }
 }
