b711fd4416bd78f44efc05549db87a3a1bdb98e8
tracer-ics2019 <tracer@njuics.org>
2022-01-05 22:34:06 +0800

    >  compile U201711354 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:34:06 up 1 day, 18:11,  1 user,  load average: 0.08, 0.08, 0.11 3f2ea59dbe93a3f4fa44fe964bd8789cdd19821d

--- nemu/src/isa/riscv32/exec/compute.c
+++ nemu/src/isa/riscv32/exec/compute.c
@@ -19,6 +19,7 @@ make_EHelper(j){
    Log("pc %x\n",decinfo.jmp_pc);
    vaddr_t r_pc=decinfo.seq_pc;
    rtl_sr(id_dest->reg,&r_pc,4);
+   print_asm_template2(j);
  
 }
 
@@ -28,6 +29,7 @@ make_EHelper(jr){
            
            vaddr_t r_pc=decinfo.seq_pc;
            rtl_sr(id_dest->reg,&r_pc,4);
+           print_asm_template2(jr);
          }
            
 make_EHelper(ri){
@@ -43,6 +45,7 @@ make_EHelper(ri){
                 result=0;                
                 rtl_add(&result,&imm,&id_src->val);
                 rtl_sr(id_dest->reg,&result,4);
+                print_asm_template2(addi);
                 break;
           case 2:
                 imm=id_src2->val;
@@ -57,7 +60,9 @@ make_EHelper(ri){
                 else{
                    rtl_sr(id_dest->reg,&result,4);
                    }
+                   print_asm_template2(slti);
                   break; 
+                  
                    
           case 3:
                 imm=id_src2->val;
@@ -72,6 +77,7 @@ make_EHelper(ri){
                 else{
                    rtl_sr(id_dest->reg,&result,4);
                    }
+                   print_asm_template2(sltiu);
                   break;
           case 4:
                 imm=id_src2->val;
@@ -79,8 +85,8 @@ make_EHelper(ri){
              //   result;                
                 rtl_xor(&result,&imm,&id_src->val);
                 rtl_sr(id_dest->reg,&result,4);
+               print_asm_template2(xori);
                 break;    
-          case 5:
                      
           case 6:
                 imm=id_src2->val;
@@ -88,6 +94,7 @@ make_EHelper(ri){
                // result;                
                 rtl_or(&result,&imm,&id_src->val);
                 rtl_sr(id_dest->reg,&result,4);
+               print_asm_template2(ori);
                 break;                
           case 7:
                 imm=id_src2->val;
@@ -95,6 +102,7 @@ make_EHelper(ri){
               //  result;                
                 rtl_and(&result,&imm,&id_src->val);
                 rtl_sr(id_dest->reg,&result,4);
+                print_asm_template2(andi);
                 break;          
 }
 
@@ -110,35 +118,41 @@ make_EHelper(b){
         case 0:
               if(id_src->val==id_src2->val)
                  rtl_j(id_dest->val+cpu.pc);
+                 print_asm_template2(beq);
                break;
 
         case 1:
               if(id_src->val!=id_src2->val)
                  rtl_j(id_dest->val+cpu.pc);
+                 print_asm_template2(bne);
                break; 
         case 4:
               src=id_src->val;
               src2=id_src2->val;
               if(src<src2)
                 rtl_j(id_dest->val+cpu.pc);
+               print_asm_template2(blt); 
               break;
         case 5:
               src=id_src->val;
               src2=id_src2->val;
               if(src>src2)
                 rtl_j(id_dest->val+cpu.pc);
+                print_asm_template2(bge);
               break; 
         case 6:
               usrc=id_src->val;
               usrc2=id_src2->val;
               if(usrc<usrc2)
                 rtl_j(id_dest->val+cpu.pc);
+                print_asm_template2(bltu);
               break;
         case 7:
               usrc=id_src->val;
               usrc2=id_src2->val;
               if(usrc>usrc2)
                 rtl_j(id_dest->val+cpu.pc);
+                print_asm_template2(bgeu);
               break;  
               }
               }
@@ -155,15 +169,21 @@ make_EHelper(r){
           case 0:
                 if(decinfo.isa.instr.funct7==0){              
                 rtl_add(&result,&id_src2->val,&id_src->val);
-                rtl_sr(id_dest->reg,&result,4);}
+                rtl_sr(id_dest->reg,&result,4);
+                print_asm_template2(add);
+                }
                 else{
                  rtl_sub(&result,&id_src2->val,&id_src->val);
-                 rtl_sr(id_dest->reg,&result,4);}
+                 rtl_sr(id_dest->reg,&result,4);
+                 print_asm_template2(sub);
+                 }
+               
                 break;
           case 1:
                 shift=id_src2->val & 0x0000001f;
                 rtl_shl(&result,&id_src->val,&shift);
                 rtl_sr(id_dest->reg,&result,4);
+               print_asm_template2(sll);
                 break;
           case 2:
                 src=id_src->val;
@@ -172,10 +192,12 @@ make_EHelper(r){
                 if(src<src2){
                    result =1;
                    rtl_sr(id_dest->reg,&result,4);
+                   
                    }
                 else{
                    rtl_sr(id_dest->reg,&result,4);
                    }
+                   print_asm_template2(slt);
                   break; 
                    
           case 3:
@@ -189,30 +211,36 @@ make_EHelper(r){
                 else{
                    rtl_sr(id_dest->reg,&result,4);
                    }
+                   print_asm_template2(sltu);
                   break; 
           case 4:             
                 rtl_xor(&result,&id_src2->val,&id_src->val);
                 rtl_sr(id_dest->reg,&result,4);
+               print_asm_template2(xor);
                 break;       
           case 5:
                 if(decinfo.isa.instr.funct7==0){              
                 shift=id_src2->val & 0x0000001f;
                 rtl_shr(&result,&id_src->val,&shift);
                 rtl_sr(id_dest->reg,&result,4);
+              print_asm_template2(srl);
                 }
                 else{
                 shift=id_src2->val & 0x0000001f;
                 rtl_sar(&result,&id_src->val,&shift);
                 rtl_sr(id_dest->reg,&result,4);
+              print_asm_template2(sra);
                  }
                 break;           
           case 6:             
                 rtl_or(&result,&id_src2->val,&id_src->val);
                 rtl_sr(id_dest->reg,&result,4);
+               print_asm_template2(or);
                 break;                
           case 7:          
                 rtl_and(&result,&id_src2->val,&id_src->val);
                 rtl_sr(id_dest->reg,&result,4);
+               print_asm_template2(and);
                 break;          
 }
 
