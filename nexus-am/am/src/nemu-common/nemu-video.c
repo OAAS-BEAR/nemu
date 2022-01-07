69262147ed5d884af9638de5c3d013106d810a2a
tracer-ics2019 <tracer@njuics.org>
2022-01-07 09:06:39 +0800

    >  run U201711354 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  09:06:39 up 37 min,  1 user,  load average: 0.21, 0.27, 0.21 43abbb867d959e02870660231f0e5ad25c576cf8

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -25,26 +25,26 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
        int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
       uint32_t *pixels = ctl->pixels;
      
-      uint32_t *fb=(uint32_t*)(FB_ADDR);
+      uint32_t *fb=(uint32_t*)(uintptr_t)(FB_ADDR);
       int W=screen_width();
       int H=screen_height();
      
-  
+  /*
         int cp_bytes = sizeof(uint32_t) * min(w, W - x);
       for (int j = 0; j < h && y + j < H; j ++) {
         memcpy(&fb[(y + j) * W + x], pixels, cp_bytes);
         pixels += w;
         }
-   
- /*    
+   */
+    
   for(int i=0;i<h;i++){
        for (int j = 0; j < w; j ++) {
             fb[(y+i)*W+j+x]=pixels[i*w+j];
       }
       }
       if (ctl->sync) {
-        //outl(SYNC_ADDR, 0);
-      }*/
+        outl(SYNC_ADDR, 0);
+      }
       return size;
     }
   }
