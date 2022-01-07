13314774574bca8635878d21c947599938d0aa5e
tracer-ics2019 <tracer@njuics.org>
2022-01-07 11:05:17 +0800

    >  compile U201711354 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:05:17 up  1:00,  1 user,  load average: 0.11, 0.07, 0.07 7aa1e7f1e6f43f91cd6bab8a46dd651eec0c3cf8

--- nemu/src/isa/riscv32/decode.c
+++ nemu/src/isa/riscv32/decode.c
@@ -81,14 +81,16 @@ make_DHelper(ld) {
   decode_op_r(id_dest, decinfo.isa.instr.rd, false);
 }
 make_DHelper(RI){
+int32_t ss=decinfo.isa.instr.simm11_0;
+ss=ss<<20>>20;
   decode_op_r(id_src, decinfo.isa.instr.rs1, true);
   if((decinfo.isa.instr.funct3==1)||(decinfo.isa.instr.funct3==5)){
  // decode_op_i(id_src2, decinfo.isa.instr.rs2, true);
-   decode_op_i(id_src2, decinfo.isa.instr.simm11_0, true);
+   decode_op_i(id_src2, ss, true);
   }
   
   else{
-  decode_op_i(id_src2, decinfo.isa.instr.simm11_0, true);
+  decode_op_i(id_src2, ss, true);
   }
   decode_op_r(id_dest, decinfo.isa.instr.rd, false);
   }
