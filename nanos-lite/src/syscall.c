c61b2f4b462f4dd90bbb99f828e36f2c5f8d9da5
tracer-ics2019 <tracer@njuics.org>
2022-01-08 20:03:19 +0800

    >  run U201711354 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:03:19 up  4:27,  1 user,  load average: 0.01, 0.06, 0.08 bc4907bdda33d4f5f8813ab14e450ccc4f92a49b

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -4,7 +4,9 @@
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
-  
+  a[1]=c->GPR2;
+  a[2]=c->GPR3;
+  a[3]=c->GPR4;
   switch (a[0]) {
     case 0://sys_exit()
      _halt(c->GPR2);
@@ -14,6 +16,16 @@ _Context* do_syscall(_Context *c) {
     _yield();
     c->GPRx=0;
     break;
+    case 4: //sys_write
+    if(a[1]==1||a[1]==2){
+       for( int i=0;i<a[3];i++){
+          _putc(*(char*)(a[2]+i));
+          }
+        c->GPRx=a[3];
+        }
+        break;
+        
+    
     default: panic("Unhandled syscall ID = %d", a[0]); break;
   }
 
