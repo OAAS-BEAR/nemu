2c310edfb2f4c16ed4cfad206b38f1c04fa0401a
tracer-ics2019 <tracer@njuics.org>
2022-01-10 04:26:03 +0800

    >  run U201711354 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  04:26:03 up 29 min,  1 user,  load average: 0.41, 0.46, 0.28 f4d05fdd65d656f8578abe2764851b733b0644e8

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -39,12 +39,12 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
   int y=(offset/4)/screen_width();
   draw_sync();
   draw_rect((uint32_t*)buf,x,y,len/4,1);
-  return 0;
+  return len;
 }
 
 size_t fbsync_write(const void *buf, size_t offset, size_t len) {
   draw_sync();
-  return 0;
+  return len;
 }
 
 void init_device() {
