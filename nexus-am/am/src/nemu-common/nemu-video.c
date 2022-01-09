f20ace06f4485844986af06fc3ca8cab6e175f37
tracer-ics2019 <tracer@njuics.org>
2022-01-10 05:20:34 +0800

    >  run U201711354 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  05:20:34 up  1:23,  1 user,  load average: 0.29, 0.18, 0.14 596dbb6b26259f30601ed297643b0ced365c0546

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -23,13 +23,16 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_FBCTL: {
       _DEV_VIDEO_FBCTL_t *ctl = (_DEV_VIDEO_FBCTL_t *)buf;
-       int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
-      uint32_t *pixels = ctl->pixels;
-     
-      uint32_t *fb=(uint32_t*)(uintptr_t)(FB_ADDR);
       int W=screen_width();
       int H=screen_height();
      
+       int x = ctl->x;
+       int y = ctl->y;
+       int w = ctl->w;
+       int h = ctl->h;
+      uint32_t *pixels = ctl->pixels;
+     
+      uint32_t *fb=(uint32_t*)(uintptr_t)(FB_ADDR);
   
         int cp_bytes = sizeof(uint32_t) * min(w, W - x);
       for (int j = 0; j < h && y + j < H; j ++) {
@@ -37,13 +40,7 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
         pixels += w;
         }
    
-    /*
-  for(int i=0;i<h;i++){
-       for (int j = 0; j < w; j ++) {
-            fb[(y+i)*W+j+x]=pixels[i*w+j];
-      }
-      }
-      */
+
       if (ctl->sync) {
         outl(SYNC_ADDR, 0);
       }
